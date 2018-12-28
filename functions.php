df04527de373d2d69199f86ae26f6e8d9b899fc7
kaeriellym <kaeriellym@gmail.com>
2018-12-28 17:10:56 -0500

    Remove site description

--- functions.php
+++ functions.php
@@ -3,18 +3,6 @@
 // Include Beans. Do not remove the line below.
 require_once( get_template_directory() . '/lib/init.php' );
 
-/*
- * Remove this action and callback function if you do not whish to use LESS to style your site or overwrite UIkit variables.
- * If you are using LESS, make sure to enable development mode via the Admin->Appearance->Settings option. LESS will then be processed on the fly.
- */
-add_action( 'beans_uikit_enqueue_scripts', 'beans_child_enqueue_uikit_assets' );
-
-function beans_child_enqueue_uikit_assets() {
-
-	beans_compiler_add_fragment( 'uikit', get_stylesheet_directory_uri() . '/style.less', 'less' );
-
-}
-
 // Remove this action and callback function if you are not adding CSS in the style.css file.
 add_action( 'wp_enqueue_scripts', 'beans_child_enqueue_assets' );
 
@@ -23,3 +11,6 @@ function beans_child_enqueue_assets() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 
 }
+
+// Remove the site title tag.
+beans_remove_action( 'beans_site_title_tag' );
