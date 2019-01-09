c22d7274f23934b5e29beb164ef50c6cb3351510
kaeriellym <kaeriellym@gmail.com>
2019-01-08 21:36:27 -0500

    Resize featured image

--- archive.php
+++ archive.php
@@ -13,6 +13,8 @@ function archive_enqueue_uikit_assets() {
 add_action( 'beans_before_load_document', 'archive_posts_grid' );
 
 function archive_posts_grid() {
+  // Move the post image above the post title.
+beans_modify_action_hook( 'beans_post_image', 'beans_post_title_before_markup' );
   // Remove post content
   beans_remove_action( 'beans_post_content');
   // Remove post meta info
@@ -26,6 +28,14 @@ function archive_posts_grid() {
   beans_wrap_markup( 'beans_post', 'archive_post_grid_column', 'div', array(
     'class' => 'uk-width-large-1-3 uk-width-medium-1-2',
   ) );
+
+  // Resize featured image
+  add_filter( 'beans_edit_post_image_args', 'myprefix_post_image_edit_args' );
+  function myprefix_post_image_edit_args( $args ) {
+      return array_merge( $args, array(
+          'resize' => array( 300, 300, true ),
+      ) );
+    }
 
   // Move the posts pagination after the new grid markup.
  beans_modify_action_hook( 'beans_posts_pagination', 'archive_posts_grid_after_markup' );
--- search.php
+++ search.php
@@ -15,6 +15,8 @@ add_action( 'beans_before_load_document', 'search_posts_grid' );
 function search_posts_grid() {
   // Remove search title
   beans_remove_action( 'beans_post_search_title');
+  // Move the post image above the post title.
+  beans_modify_action_hook( 'beans_post_image', 'beans_post_title_before_markup' );
   // Remove post content
   beans_remove_action( 'beans_post_content');
   // Remove post meta info
@@ -28,6 +30,14 @@ function search_posts_grid() {
   beans_wrap_markup( 'beans_post', 'search_post_grid_column', 'div', array(
     'class' => 'uk-width-large-1-3 uk-width-medium-1-2',
   ) );
+
+  // Resize featured image
+  add_filter( 'beans_edit_post_image_args', 'myprefix_post_image_edit_args' );
+  function myprefix_post_image_edit_args( $args ) {
+      return array_merge( $args, array(
+          'resize' => array( 300, 300, true ),
+      ) );
+    }
 
   // Move the posts pagination after the new grid markup.
  beans_modify_action_hook( 'beans_posts_pagination', 'search_posts_grid_after_markup' );
