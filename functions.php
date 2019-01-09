1a78d12f67d27b4650f503ee55cd4b276185b62c
kaeriellym <kaeriellym@gmail.com>
2019-01-08 19:30:21 -0500

    Add dynamic grid to archive and search

--- functions.php
+++ functions.php
@@ -30,6 +30,7 @@ function secondary_widget_area() {
 
 }
 
+
 // Register a footer widget area.
 add_action( 'widgets_init', 'footer_widget_area' );
 
@@ -58,6 +59,7 @@ function display_footer_widget_area() {
 
 }
 
+
 // Modify the read more text.
 add_filter( 'beans_post_more_link_text_output', 'example_modify_read_more' );
 
@@ -66,6 +68,7 @@ function example_modify_read_more() {
    return 'Read more';
 
 }
+
 
 // Remove post author
 add_filter( 'beans_post_meta_items', 'beans_child_remove_post_meta_items' );
@@ -82,15 +85,13 @@ function beans_child_remove_post_meta_items( $items ) {
 beans_remove_output( 'beans_post_meta_date_prefix' );
 beans_remove_output( 'beans_post_meta_categories_prefix' );
 beans_remove_output( 'beans_post_meta_tags_prefix' );
-
 // Remove "No comment yet" text
 beans_remove_action( 'beans_no_comment' );
-
 // Remove symbol after read more text
 beans_remove_markup( 'beans_next_icon[_more_link]' );
-
 // Remove archive titles
 beans_remove_action( 'beans_post_archive_title' );
+
 
 // Modify the "Previous" post navigation text.
 add_filter( 'beans_previous_text_post_navigation_output', 'example_previous_text_post_navigation' );
@@ -118,6 +119,7 @@ function example_next_text_post_navigation( $text ) {
 
 }
 
+
 // Enqueue uikit sticky
 add_action( 'beans_uikit_enqueue_scripts', 'uikit_addon_sticky');
 
@@ -132,5 +134,49 @@ add_action( 'beans_before_load_document', 'beans_sticky_header');
 function beans_sticky_header() {
 
 	beans_add_attribute( 'beans_header', 'data-uk-sticky', "{top:0}" );
+
+}
+
+
+// Display posts in responsive dynamic grid on archive & search pages
+// Enqueue the UIkit dynamic grid component.
+add_action( 'beans_uikit_enqueue_scripts', 'example_enqueue_grid_uikit_assets' );
+
+function example_enqueue_grid_uikit_assets() {
+
+  // Only apply to non singular view.
+ if ( is_archive() || is_search() ) {
+   beans_uikit_enqueue_components( array( 'grid' ), 'add-ons' );
+ }
+
+}
+
+// Display posts in a responsive dynamic grid.
+add_action( 'wp', 'example_posts_grid' );
+
+function example_posts_grid() {
+
+  // Only apply to archive and search
+ if ( is_archive() || is_search() ) {
+	 // Remove search page title
+	 beans_remove_action( 'beans_post_search_title');
+	 // Remove post content
+	 beans_remove_action( 'beans_post_content');
+   // Remove post meta info
+   beans_remove_action( 'beans_post_meta' );
+	 beans_remove_action( 'beans_post_meta_tags' );
+	 beans_remove_action( 'beans_post_meta_categories' );
+   // Add grid.
+    beans_wrap_inner_markup( 'beans_content', 'beans_child_posts_grid', 'div', array(
+     'data-uk-grid' => '{gutter: 20}'
+    ) );
+    beans_wrap_markup( 'beans_post', 'beans_child_post_grid_column', 'div', array(
+      'class' => 'uk-width-large-1-3 uk-width-medium-1-2'
+   ) );
+
+    // Move the posts pagination after the new grid markup.
+   beans_modify_action_hook( 'beans_posts_pagination', 'beans_child_posts_grid_after_markup' );
+
+  }
 
 }
