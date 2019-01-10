ee71abd7f476fb9bab36ce80c27fe37df2e73357
kaeriellym <kaeriellym@gmail.com>
2019-01-10 11:36:38 -0500

    Fix function to display posts on 404 page

--- 404.php
+++ 404.php
@@ -5,19 +5,22 @@ beans_modify_action_callback( 'beans_no_post', 'my_new_no_post' );
  * New callback template to replace the default 'beans_no_post'.
  */
 function my_new_no_post() {
+  	// Get the 6 latests posts.
+  	$posts = get_posts( array( 'numberposts' => 4 ) );
 
-  $recent_posts = wp_get_recent_posts(array(
-        'numberposts' => 3 // Number of recent posts thumbnails to display
+  	foreach ( $posts as $post ) {
+  		// Setup the postdata.
+  		global $post;
+  		setup_postdata( $post );
 
-    ));
-      foreach( $recent_posts as $recent ) {
-          printf( '<li><a href="%1$s">%2$s</a></li>',
-              esc_url( get_permalink( $recent['ID'] ) ),
-              apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
-          );
-      }
+  		// Display post title and image using Beans fragments.
+  		beans_post_title();
+  		beans_post_image();
+  	}
 
 }
+
+
 
 // Load the document
 beans_load_document();
