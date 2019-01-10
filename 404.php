53cb5a003ae769d49453f44616681200e4832a11
kaeriellym <kaeriellym@gmail.com>
2019-01-10 12:43:43 -0500

    Refactor recent posts on 404 page

--- 404.php
+++ 404.php
@@ -4,23 +4,29 @@ beans_modify_action_callback( 'beans_no_post', 'my_new_no_post' );
 /*
  * New callback template to replace the default 'beans_no_post'.
  */
-function my_new_no_post() {
-  	// Get the 6 latests posts.
-  	$posts = get_posts( array( 'numberposts' => 4 ) );
+ function my_new_no_post() {
+   	// Get the 6 latests posts.
+   	$posts = get_posts( array( 'numberposts' => 3 ) );
+     ?>
+     <div class="uk-grid">
 
-  	foreach ( $posts as $post ) {
-  		// Setup the postdata.
-  		global $post;
-  		setup_postdata( $post );
+     <?php
+   	foreach ( $posts as $post ) {
+   		// Setup the postdata.
+   		global $post;
+   		setup_postdata( $post ); ?>
+         <div class="uk-width-large-1-3 uk-width-medium-1-2">
+           <?php
+             beans_post_title();
+             beans_post_image();
+           ?>
 
-  		// Display post title and image using Beans fragments.
-  		beans_post_title();
-  		beans_post_image();
-  	}
+         </div> <?php
 
 }
+?> </div> <?php
 
-
+}
 
 // Load the document
 beans_load_document();
