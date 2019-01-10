ecbcf7fd7c0926393a38d0a0e574a5e4fb559226
kaeriellym <kaeriellym@gmail.com>
2019-01-10 16:05:40 -0500

    Fix 404 most recent post bug

--- 404.php
+++ 404.php
@@ -3,29 +3,37 @@
 add_action( 'beans_content', 'recent_posts_grid' );
 
 function recent_posts_grid() {
-  // Get the 3 latest posts
-  $posts = get_posts( array( 'numberposts' => 6 ) );
+
+  // Get latest posts
+  $custom_posts = get_posts( array( 'numberposts' => 6 ) );
+
   // Remove post meta
   beans_remove_action( 'beans_post_meta' );
+
   // Add responsive grid
   ?><div id="recent-posts-grid" class="uk-grid uk-grid-match" data-uk-grid-margin=""><?php
-  foreach ( $posts as $post ) {
+
+  // Global variable 
+  global $post;
+
+  // Add posts
+  foreach ( $custom_posts as $post ) {
+
     // Setup the postdata.
-    global $post;
     setup_postdata( $post );
-    ?>
-    <div class="uk-width-large-1-3 uk-width-medium-1-2">
-    <article class="uk-article uk-panel-box">
-      <?php
-      beans_post_image();
-      beans_post_title();
-      ?>
-    </article></div>
-    <?php
+    ?><div class="uk-width-large-1-3 uk-width-medium-1-2">
+    <article class="uk-article uk-panel-box"><?php
+
+    // Add post image and title
+    beans_post_image();
+    beans_post_title();
+
+    ?></article></div><?php
+
   }
-  ?>
-  </div>
-  <?php
+
+  ?></div><?php
+
 }
 
 // Resize featured image
