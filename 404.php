2aa7fd6b84ae635cf00a407b55dd68d0a97a484a
kaeriellym <kaeriellym@gmail.com>
2019-01-10 14:24:18 -0500

    Add formatting to grid on 404 page

--- 404.php
+++ 404.php
@@ -1,31 +1,41 @@
 <?php
 
-beans_modify_action_callback( 'beans_no_post', 'my_new_no_post' );
-/*
- * New callback template to replace the default 'beans_no_post'.
- */
- function my_new_no_post() {
-   	// Get the 6 latests posts.
-   	$posts = get_posts( array( 'numberposts' => 3 ) );
-     ?>
-     <div class="uk-grid">
+beans_modify_action_callback( 'beans_no_post', 'recent_posts_grid' );
 
-     <?php
-   	foreach ( $posts as $post ) {
-   		// Setup the postdata.
-   		global $post;
-   		setup_postdata( $post ); ?>
-         <div class="uk-width-large-1-3 uk-width-medium-1-2">
-           <?php
-             beans_post_title();
-             beans_post_image();
-           ?>
+function recent_posts_grid() {
+  // Get the 3 latest posts
+  $posts = get_posts( array( 'numberposts' => 6 ) );
+  // Remove post meta
+  beans_remove_action( 'beans_post_meta' );
+  // Add responsive grid
+  ?><div class="uk-grid uk-grid-match" data-uk-grid-margin=""><?php
+  foreach ( $posts as $post ) {
+    // Setup the postdata.
+    global $post;
+    setup_postdata( $post );
+    ?>
+    <div class="uk-width-large-1-3 uk-width-medium-1-2">
+    <article class="uk-article uk-panel-box">
 
-         </div> <?php
 
+    <?php
+      beans_post_image();
+      beans_post_title();
+      ?>
+
+    </article></div>
+
+  <?php
+  }
+  ?> </div> <?php
 }
-?> </div> <?php
 
+// Resize featured image
+add_filter( 'beans_edit_post_image_args', 'gpeach_post_image_edit_args' );
+function gpeach_post_image_edit_args( $args ) {
+  return array_merge( $args, array(
+    'resize' => array( 500, 500, true ),
+    ) );
 }
 
 // Load the document
